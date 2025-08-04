1bc3f827528a7f6d7ec98a1c93a57b46690d1e96
Javier <javier@queryout.com>
2025-08-04 20:01:56 +1000

    Adding solution

--- src/get-arnie-quotes.js
+++ src/get-arnie-quotes.js
@@ -1,10 +1,30 @@
 const { httpGet } = require('./mock-http-interface');
 
-const getArnieQuotes = async (urls) => {
-  // TODO: Implement this function.
-  // return results;
+const parseResponse = (response) => {
+    const { message } = JSON.parse(response.body);
+
+    if (response.status === 200) {
+        return { 'Arnie Quote': message };
+    }
+
+    return { FAILURE: message };
+};
+
+const handleRequest = async (url) => {
+    try {
+        const response = await httpGet(url);
+        return parseResponse(response);
+    } catch (error) {
+        return { FAILURE: error.message || 'Unknown error' };
+    }
+};
+
+const getArnieQuotes = (urls) => {
+    const quotePromises = urls.map((url) => handleRequest(url));
+    
+    return Promise.all(quotePromises);
 };
 
 module.exports = {
-  getArnieQuotes,
+    getArnieQuotes,
 };
